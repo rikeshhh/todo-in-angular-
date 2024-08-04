7fb6b61a21c022cbcb89f27c57eee782eadcf3f6
Rikesh Shrestha <114804728+RikeshShresth@users.noreply.github.com>
2024-08-04 12:04:32 +0545

    chore: Update package dependencies and add CRUD functionality for todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
 })
-export class AppComponent {
-  title = 'todo-in-anglr';
+export class AppComponent implements OnInit {
+  title = 'todo-app';
+  todos: any[] = [];
+  newTodo = '';
+
+  private apiUrl = 'http://localhost:3000/todos';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchTodos();
+  }
+
+  fetchTodos() {
+    this.http.get<any[]>(this.apiUrl).subscribe((data) => {
+      this.todos = data;
+    });
+  }
+
+  deleteTodo(id: number) {
+    this.http.delete(`${this.apiUrl}/${id}`).subscribe(() => {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  addTodo() {
+    if (this.newTodo.trim().length === 0) {
+      return;
+    }
+    const newTodo = {
+      text: this.newTodo,
+      completed: false,
+      timestamp: new Date(),
+    };
+    this.http.post<any>(this.apiUrl, newTodo).subscribe((data) => {
+      this.todos.push(data);
+      this.newTodo = '';
+    });
+  }
 }
